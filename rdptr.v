7d69ccc307e9ceea4c0065b0b8f707e929946a9d
deva30123 <devajothi.sardar3120@gmail.com>
2025-09-29 15:41:19 +0530

    Update rdptr.v

--- rdptr.v
+++ rdptr.v
@@ -1,4 +1,4 @@
-module rptr #(parameter PTR_WIDTH=3) (
+module rdptr #(parameter PTR_WIDTH=3) (
   input rclk, rrst_n, r_en,
   input [PTR_WIDTH:0] g_wptr_sync,
   output reg [PTR_WIDTH:0] b_rptr, g_rptr,
