f4bcf47ed9e89275b44ea23c8d3ea2586899b282
deva30123 <devajothi.sardar3120@gmail.com>
2025-11-05 15:59:43 +0530

    Enhance pointer module with parameterized width
    Refactor pointer module to include parameterized width and additional logic.

--- module/pointer.v
+++ module/pointer.v
@@ -1,10 +1,20 @@
+parameter ptr_w = 3;
 module pointer(
   input en,clk,
-  output [2:0] nxp_g, ptr,
+  input reg [ptr_w-1:0] pb_g,
+  output [ptr_w-1:0] ptr pa_g,
   output stat
 );
-  reg [2:0] p;
-  always@(posedge clk)begin
-    p <= p+1;
+  int i;
+  reg [ptr_w-1:0] pb_b,pa_a;
+  reg rank;
+  always@(*)begin
+    if(pb_b > a) rank = 0;
+    else if(pb_b < a) rank = 1;
+    else(pb_b == a) rank = rank;
   end
+  always@(*)begin
+    for(i=ptr_w;)     
+  end
+  
 end module
