98406cf941c12147008fe1e16bd05e513c3e6562
deva30123 <devajothi.sardar3120@gmail.com>
2025-11-05 16:05:11 +0530

    Refactor pointer module for output and logic fixes
    Refactor pointer module to correct output declaration and update rank logic. Adjust gray to binary conversion loop.

--- module/pointer.v
+++ module/pointer.v
@@ -2,19 +2,22 @@ parameter ptr_w = 3;
 module pointer(
   input en,clk,
   input reg [ptr_w-1:0] pb_g,
-  output [ptr_w-1:0] ptr pa_g,
+  output [ptr_w-1:0] ptr, pa_g,
   output stat
 );
   int i;
-  reg [ptr_w-1:0] pb_b,pa_a;
+  reg [ptr_w-1:0] pb_b,pa_b;
   reg rank;
   always@(*)begin
-    if(pb_b > a) rank = 0;
-    else if(pb_b < a) rank = 1;
-    else(pb_b == a) rank = rank;
+    if(pb_b > a) rank <= 0;
+    else if(pb_b < a) rank <= 1;
+    else(pb_b == a) rank <= rank;
   end
-  always@(*)begin
-    for(i=ptr_w;)     
+  always@(*)begin//gray to binary
+    for(i=ptr_w;i>0;i=i-1)begin
+      pb_b[i] = pb_g[i]^pb_b[i+1];
+    end
   end
+  assign pa_g = pa_b^{0,pa_b[ptr_w-1:1]};
   
 end module
